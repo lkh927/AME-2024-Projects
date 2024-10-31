ae3bf2c67e3bf7d295bc926b7fc2a3f2f289d16c
annaabildskov <bvh632@alumni.ku.dk>
2024-10-31 12:08:05 +0100

    lots of stuff

---
+++ Project 2/Project_2.py
@@ -0,0 +1,108 @@
+import numpy as np
+import numpy.linalg as la
+import pandas as pd
+from sklearn.linear_model import Lasso
+from scipy.stats import norm
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+
+''' Importing data '''
+dat = pd.read_csv('growth.csv')
+lbldf = pd.read_csv('labels.csv', index_col='variable')
+lbl_all = lbldf.label.to_dict() # as a dictionary
+
+def filter_data(dat):
+    ''' A function to filter the data and investigate missing values.
+    Inputs = data set to filter 
+    Output = missing data counts for each region and all data '''
+
+    # Filter the dataset to include only rows where 'gdp_growth' is non-missing
+    filtered_dat = dat[dat[['gdp_growth', 'lgdp_initial']].notnull().all(axis=1)]
+
+    # Count the number of times all variables in the filtered dataset are non-missing
+    non_missing_counts = filtered_dat.notnull().sum()
+    # Sort non_missing_counts from lowest to highest
+    sorted_non_missing_counts = non_missing_counts.sort_values()
+
+    # Do the same for each region (replicating the sorting in "sorted_non_missing_counts")
+    regions = ['africa', 'americas', 'asia', 'europe', 'oceania']
+    sorted_non_missing_counts_by_region = {}
+
+    for region in regions:
+        region_subset = filtered_dat[filtered_dat[region] == 1]
+        non_missing_counts_region = region_subset.notnull().sum()
+        sorted_non_missing_counts_by_region[region] = non_missing_counts_region[sorted_non_missing_counts.index]
+
+    # Extract the sorted non-missing counts for each region
+    sorted_non_missing_counts_africa = sorted_non_missing_counts_by_region['africa']
+    sorted_non_missing_counts_americas = sorted_non_missing_counts_by_region['americas']
+    sorted_non_missing_counts_asia = sorted_non_missing_counts_by_region['asia']
+    sorted_non_missing_counts_europe = sorted_non_missing_counts_by_region['europe']
+    sorted_non_missing_counts_oceania = sorted_non_missing_counts_by_region['oceania']
+
+    # Create a DataFrame to present the non-missing counts for each region
+    non_missing_counts_df = pd.DataFrame({
+        'Variable': sorted_non_missing_counts.index,
+        'All': sorted_non_missing_counts.values,
+        'Africa': sorted_non_missing_counts_africa.values,
+        'Americas': sorted_non_missing_counts_americas.values,
+        'Asia': sorted_non_missing_counts_asia.values,
+        'Europe': sorted_non_missing_counts_europe.values,
+        'Oceania': sorted_non_missing_counts_oceania.values
+    })
+
+    # Display the DataFrame
+    print('Filtered data displayed by non-missing values for each region:')
+    print(non_missing_counts_df.to_string(index=False))
+    print(f'Data contains {len(non_missing_counts_df)} variables')
+
+    return
+
+def group_data(dat):
+    ''' A function to group variables by type, e.g. institutional or geographical variables. '''
+
+    # all available variables
+    vv_outcome = ['gdp_growth']
+    vv_key_explanatory = ['lgdp_initial']
+
+    vv_institutions = ['dem', 'demCGV', 'demBMR', 'demreg', 
+                    'currentinst', 'polity', 'polity2'] 
+    vv_geography = ['tropicar','distr', 'distcr', 'distc','suitavg','temp', 'suitgini', 'elevavg', 'elevstd',
+                    'kgatr', 'precip', 'area', 'abslat', 'cenlong', 'area_ar', 'rough','landlock', 
+                    'africa',  'asia', 'oceania', 'americas']
+    vv_geneticdiversity = ['pdiv', 'pdiv_aa']
+    vv_historical = ['pd1000', 'pd1500', 'pop1000', 'pop1500', 'ln_yst', 'ln_yst_aa', #NOTE THAT PD1500 OCCURS TWICE IN THE DATA ('PD1500' AND 'PD1500.1'). WE ONLY INCLUDE ONE HERE
+                    'legor_fr', 'legor_uk', 'pd1', 'pop1', 'pop_growth', 'population_initial','population_now']
+    vv_religion = ['pprotest', 'pcatholic', 'pmuslim']
+    vv_danger = ['yellow', 'malfal',  'uvdamage']
+    vv_resources = ['oilres', 'goldm', 'iron', 'silv', 'zinc']
+    vv_educ = ['ls_bl', 'lh_bl'] 
+    vv_economic = ['investment_rate', 'capital_growth_pct_gdp_initial', 'capital_growth_pct_gdp_now', 
+                'gdp_now', 'gdp_pc_initial', 'gdp_pc_now', 'ginv', 'marketref']
+
+                #due to large number of missing observations:
+    vv_excluded = ['leb95', 'imr95', 'mortality', 'imputedmort', 'logem4', 
+                'lt100km', 'excolony', 'democ00a', 'democ1', 'cons00a',
+                'pdivhmi', 'pdivhmi_aa',
+                #due to lack of relevance or perfect correlation with included variables:
+                    'code', 'gdp_initial', 'lpop_initial',
+                #reference variables:
+                    'pother', 'europe', 'lp_bl']
+
+    vv_all = {'institutions': vv_institutions, 
+            'geography': vv_geography, 
+            'geneticdiversity': vv_geneticdiversity,
+            'historical': vv_historical,
+            'religion': vv_religion,
+            'danger':vv_danger, 
+            'resources':vv_resources,
+            'educ': vv_educ,
+            'economic': vv_economic,
+            }
+
+    return vv_outcome, vv_key_explanatory, vv_excluded, vv_all
+
+
+
+
